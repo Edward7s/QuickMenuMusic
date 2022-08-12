398efa5be8ff0af4a39d38a87855f99f2adf2658
Edward7 <ediro174@gmail.com>
2022-08-12 17:56:46 +0300

    Add project files.

---
+++ QuickMenuMusic/Config.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Net;
+using System.Linq;
+namespace QuickMenuMusic
+{ 
+    internal class Config
+    {
+        public static Config Instance { get; private set; }
+        public string Path { get; private set; }
+        public Config()
+        {
+            Instance = this;
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + "//Nocturnal"))
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "//Nocturnal");
+
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic"))
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic");
+
+            if (Directory.GetFiles(Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic").Length != 0)
+            {
+                Path = Directory.GetFiles(Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic").FirstOrDefault();
+                return;
+            }
+
+            using (WebClient wc = new WebClient()) 
+                wc.DownloadFile("", Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic");
+
+            Path = Directory.GetFiles(Directory.GetCurrentDirectory() + "//Nocturnal//MenuMusic").FirstOrDefault();
+
+        }
+    }
+}
---
+++ QuickMenuMusic/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UIElements;
+
+namespace QuickMenuMusic
+{
+    public class Main : MelonMod
+    {
+        private ABI_RC.Core.InteractionSystem.CVR_MenuManager _qmManager { get; set; }
+        private ABI_RC.Core.InteractionSystem.ViewManager _bigMenu { get; set; }
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(WaitForUi());
+            new Config();
+        }
+
+        private static IEnumerator WaitForUi()
+        {
+            while (GameObject.Find("/Cohtml") == null) yield return null;
+
+            AudioSource audioSource = new GameObject("MenuMusicManager").AddComponent<AudioSource>();
+            audioSource.transform.parent = GameObject.Find("/Cohtml").transform;
+            WWW webReq = new WWW("File://" + Config.Instance.Path);
+            yield return webReq;
+            audioSource.clip = webReq.GetAudioClip(false, false);
+            audioSource.playOnAwake = true;
+        }
+    }
+}
